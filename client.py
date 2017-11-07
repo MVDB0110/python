5ab50bb1f35a205b47f2fdc9a8297a083282f3d3
Mike van den Brink <mbrink1999@outlook.com>
2017-11-07 13:22:47 +0100

    berichten versturen en ontvangen

--- client.py
+++ client.py
@@ -1,6 +1,18 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from verzenden import *
+import socket
+
+def stuur_bericht(bericht):
+    while True:
+        c, addr = s.accept()
+        print('Ik heb verbinding met: ', addr)
+        c.send(bericht)
+        c.close()
+        break
+
+s = socket.socket()
+s.bind(('', 12345))
+s.listen(5)
 
 rood = 21
 geel = 24
--- server.py
+++ server.py
@@ -1,4 +1,16 @@
-from ontvangen import *
+import socket
+from time import sleep
 
-print(socket.gethostname())
+def ontvangen():
+    while True:
+        try:
+            s = socket.socket()
+            s.connect(('192.168.42.1', 12345))
+            s.close
+            return s.recv(1024)
+            break
+        except:
+            print('Geen alarmcode ontvangen.')
+            sleep(15)
+
 print(ontvangen())
