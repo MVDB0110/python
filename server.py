e0516506b823ac255a45ed091c28eedcdb97312e
Mike van den Brink <mbrink1999@outlook.com>
2017-11-08 15:11:16 +0100

    Loop weg voor php

--- server.py
+++ server.py
@@ -5,15 +5,14 @@ from time import sleep
 host = 'raspbian_mike'
 
 def ontvangen():
-    while True:
-        try:
-            s = socket.socket()
-            s.connect((host, 12345))
-            s.close
-            return s.recv(1024)
-            break
-        except:
-            print('Geen alarmcode ontvangen.')
-            sleep(15)
+    try:
+        s = socket.socket()
+        s.connect((host, 12345))
+        s.close
+        return s.recv(1024)
+
+    except:
+        return '0'
+        sleep(15)
 
 print(json.dumps(ontvangen()))
