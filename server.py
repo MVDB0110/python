4036c775ddec5fec075c9d7cabe63febab13f49e
Mike van den Brink <mbrink1999@outlook.com>
2017-11-08 18:51:07 +0100

    Alarm bericht op server (niet afzetten)

--- server.py
+++ server.py
@@ -3,18 +3,17 @@ import threading
 from tkinter import *
 from time import sleep
 
-host = 'raspbian_mike'
-
 root = Tk()
 root.title("Dashboard")
+
+host = 'raspbian_mike'
 var = StringVar()
 
-def init():
-    text = Label(root, textvariable=var)
-    text.pack()
+def knop_gedrukt():
+    threadontvang.start()
+    button.pack_forget()
 
 def ontvangen():
-    threading.Timer(15.0, ontvangen).start()
     try:
         s = socket.socket()
         s.connect((host, 12345))
@@ -22,11 +21,18 @@ def ontvangen():
         s.close()
         var.set("Alarm gaat af op client.")
         print("Alarm gaat af op client.")
+        threadontvang.cancel()
+        button.pack()
 
     except:
         var.set("Alarm gaat niet af op client.")
         print("Alarm gaat niet af op client.")
 
-ontvangen()
-init()
+threadontvang = threading.Timer(15.0, ontvangen)
+threadontvang.start()
+
+button = Button(root, text="Alarm is afgezet.", command=knop_gedrukt)
+text = Label(root, textvariable=var)
+text.pack()
+
 root.mainloop()
